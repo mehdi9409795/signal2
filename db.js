5453f05cd25e8a32f32969e272c9a610c2c67d59
mehdi9409795 <m.mirzaei@gardenoflove.co>
2025-10-24 17:47:17 +0330

    complete

---
+++ db.js
@@ -0,0 +1,39 @@
+const { MongoClient, ServerApiVersion } = require("mongodb");
+const dotenv = require("dotenv");
+
+dotenv.config();
+// const uri = `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@signal.eykiv6q.mongodb.net/?appName=signal`;
+
+const uri = `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@signal.eykiv6q.mongodb.net/main_db?appName=signal`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+let db;
+
+async function connectDB() {
+  if (!db) {
+    try {
+      await client.connect();
+      db = client.db("main_db"); // replace with your DB name
+      console.log("MongoDB connected");
+    } catch (err) {
+      console.error("MongoDB connection error:", err);
+    }
+  }
+  return db;
+}
+
+async function getCollection(collectionName) {
+  const database = await connectDB();
+  return database.collection(collectionName);
+}
+
+module.exports = { getCollection };
--- index.js
+++ index.js
@@ -1,7 +1,11 @@
-const fs = require("fs");
+const getCollection = require("./db").getCollection;
+const dotenv = require("dotenv");
 
-const BOT_TOKEN = "7963519635:AAGDvohFhFMtnE4wbHy9icnxuUqPtkeKBZc";
-const CHAT_ID = "85395051";
+dotenv.config();
+
+const BOT_TOKEN = process.env.BOT_TOKEN;
+const CHAT_ID = process.env.CHAT_ID;
+const API_KEY = process.env.API_KEY;
 
 const timeFrameMsMap = {
   "5min": 5 * 60 * 1000,
@@ -34,11 +38,9 @@ function calculateATR(candles, period = 14) {
   const n = candles.length;
   if (n === 0) return [];
   if (n <= period) {
-    // برای تعداد کم کندل نمیشه ATR محاسبه کرد؛ برمی‌گردونیم آرایه با NaN
     return Array(n).fill(NaN);
   }
 
-  // محاسبه‌ی TR برای هر کندل (TR برای کندل i براساس high[i], low[i], close[i-1])
   const TR = new Array(n).fill(NaN);
   for (let i = 1; i < n; i++) {
     const high = Number(candles[i].high);
@@ -52,10 +54,8 @@ function calculateATR(candles, period = 14) {
     TR[i] = tr;
   }
 
-  // آرایهٔ خروجی ATR هم‌طول با کندل‌ها؛ مقدار در ایندکس i نشانگر ATR برای کندل i هست
   const ATR = new Array(n).fill(NaN);
 
-  // مقدار اولیه: میانگین ساده‌ی TRها از i = 1 تا i = period
   let sum = 0;
   for (let i = 1; i <= period; i++) {
     sum += TR[i];
@@ -63,51 +63,27 @@ function calculateATR(candles, period = 14) {
   const firstAtr = sum / period;
   ATR[period] = firstAtr;
 
-  // حالا از i = period+1 تا آخر با فرمول Wilder ادامه می‌دهیم
   for (let i = period + 1; i < n; i++) {
-    // ATR[i] مربوط به کندل i است و براساس ATR[i-1] و TR[i] محاسبه می‌شود
     ATR[i] = (ATR[i - 1] * (period - 1) + TR[i]) / period;
   }
 
   return ATR;
 }
 
-const FILE_PATH = "./storedData.json";
 const timeFrame = "15min";
 const symbols = ["bnb/usd", "eth/usd", "xaut/usd"];
-function loadRecords() {
-  if (!fs.existsSync(FILE_PATH)) return {};
-  try {
-    const data = JSON.parse(fs.readFileSync(FILE_PATH, "utf8"));
-    return data || {};
-  } catch {
-    return {};
-  }
-}
+let records = [];
 
-function saveRecords(records) {
-  fs.writeFileSync(FILE_PATH, JSON.stringify(records));
-}
+async function addRecord(newItem, collection) {
+  const isDuplicate = await collection.findOne({
+    timeFrame: newItem.timeFrame,
+    symbol: newItem.symbol,
+    price: newItem.price,
+  });
 
-let records = loadRecords();
-
-function isDuplicate(newItem, items) {
-  return items?.some(
-    (item) =>
-      item.timeFrame === newItem.timeFrame &&
-      item.symbol === newItem.symbol &&
-      item.price === newItem.price
-  );
-}
-
-function addRecord(newItem) {
-  const items = records[newItem.symbol] || [];
-  if (!isDuplicate(newItem, items)) {
-    items?.push(newItem);
-    if (items?.length > 50) items?.shift();
-    records = { ...records, [newItem.symbol]: items };
-    saveRecords(records);
-    sendTelegramMessage(
+  if (!isDuplicate) {
+    await collection.insertOne(newItem);
+    await sendTelegramMessage(
       newItem.symbol +
         "\n" +
         newItem.timeFrame +
@@ -116,36 +92,48 @@ function addRecord(newItem) {
         "\n" +
         newItem.time
     );
-    return true;
+    const counts = await collection.count();
+
+    if (counts > 40) {
+      const oldest = await collection.findOne({}, { sort: { _id: 1 } });
+      if (oldest) {
+        await collection.deleteOne({ _id: oldest._id });
+      }
+    }
   }
-  return false;
 }
 
 function getRecords() {
   return records;
 }
 
-const findSignal = (data, symbol) => {
+const findSignal = async (data, symbol) => {
   const atrs = calculateATR(data);
-  data?.map((candle, index) => {
+  const collection = await getCollection(symbol);
+  let index = 0;
+  for (const candle of data) {
     const bodyCandle = Math.abs(candle.close - candle.open);
     if (bodyCandle >= atrs[index] * 2) {
       const date = new Date(candle.time * 1000 + 1000 * 60 * 60 * 3.5);
       const hours = date.getHours();
       const minutes = date.getMinutes();
-      addRecord({
-        timeFrame,
-        symbol,
-        price: (candle.open + candle.close) / 2,
-        time: `${hours}:${minutes}`,
-      });
+      await addRecord(
+        {
+          timeFrame,
+          symbol,
+          price: (candle.open + candle.close) / 2,
+          time: `${hours}:${minutes}`,
+        },
+        collection
+      );
     }
-  });
+    index += 1;
+  }
 };
 
 async function getDataFromService(symbol) {
   const res = await fetch(
-    `https://api.twelvedata.com/time_series?symbol=${symbol}&outputsize=500&base_exchange=bybit&interval=${timeFrame}&apikey=5bbad3eb48cf4ed98433686a07ebb895`
+    `https://api.twelvedata.com/time_series?symbol=${symbol}&outputsize=500&base_exchange=bybit&interval=${timeFrame}&apikey=${API_KEY}`
   );
   const data = await res.json();
 
@@ -157,12 +145,21 @@ async function getDataFromService(symbol) {
     close: parseFloat(candle.close),
   }));
 
-  findSignal(candles.reverse(), symbol);
+  await findSignal(candles.reverse(), symbol);
 }
 
 async function mapForSymbols() {
-  for (const symbol of symbols) await getDataFromService(symbol);
+  for (const symbol of symbols) {
+    await getDataFromService(symbol);
+  }
   console.log("is Complete");
 }
-mapForSymbols();
-setInterval(mapForSymbols, timeFrameMsMap[timeFrame]);
+
+(async () => {
+  try {
+    await mapForSymbols();
+    // setInterval(mapForSymbols, timeFrameMsMap[timeFrame]);
+  } catch (err) {
+    console.error("Error:", err);
+  }
+})();
